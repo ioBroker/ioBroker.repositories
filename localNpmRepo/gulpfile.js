b0a2820c891cdf45929f9f064a82287203d400d6
GermanBluefox <dogafox@gmail.com>
2017-10-22 17:43:30 +0200

    (bluefox) tune local npm

--- localNpmRepo/gulpfile.js
+++ localNpmRepo/gulpfile.js
@@ -44,6 +44,13 @@ function createStableRepo(done) {
             }
         }
     }
+    for (let a in latest) {
+        if (latest.hasOwnProperty(a)) {
+            if (!pack.dependencies['iobroker.' + a]) {
+                delete latest[a];
+            }
+        }
+    }
     if (!fs.existsSync(__dirname + '/public')) {
         fs.mkdirSync(__dirname + '/public');
     }    // update versions
@@ -52,37 +59,54 @@ function createStableRepo(done) {
     }
 
     fs.writeFileSync(__dirname + '/public/sources-dist-stable.json', JSON.stringify(stable, null, 2));
+    fs.writeFileSync(__dirname + '/public/sources-dist.json', JSON.stringify(latest, null, 2));
 
-    fs.writeFileSync(__dirname + '/ioBroker/package.json', JSON.stringify(pack, null, 2));
+    fs.writeFileSync(__dirname + '/ioBroker/package-latest.json', JSON.stringify(require(__dirname + '/packageProd.json'), null, 2));
+    fs.writeFileSync(__dirname + '/ioBroker/package-stable.json', JSON.stringify(pack, null, 2));
 
-    tools.getRepositoryFile(__dirname + '/public/sources-dist-stable.json', latest, function (err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        // get all icons
-        let list = [];
-        for (let i in data) {
-            if (!data.hasOwnProperty(i) || !data[i].extIcon) continue;
-            list.push({url: data[i].extIcon, name: 'logo-' + i.toLowerCase() + '.png'});
-            data[i].extIcon = '/imgs/logo-' + i.toLowerCase() + '.png';
-        }
+    tools.getRepositoryFile(__dirname + '/public/sources-dist.json', latest, function (err, dataLatest) {
+        tools.getRepositoryFile(__dirname + '/public/sources-dist-stable.json', latest, function (err, data) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            // get all icons
+            let list = [];
+            for (let i in data) {
+                if (!data.hasOwnProperty(i) || !data[i].extIcon) continue;
+                list.push({url: data[i].extIcon, name: 'logo-' + i.toLowerCase() + '.png'});
+                data[i].extIcon = '/imgs/logo-' + i.toLowerCase() + '.png';
+            }
 
 
-        if (!fs.existsSync(__dirname + '/public/imgs')) {
-            fs.mkdirSync(__dirname + '/public/imgs');
-        }
+            if (!fs.existsSync(__dirname + '/public/imgs')) {
+                fs.mkdirSync(__dirname + '/public/imgs');
+            }
 
-        console.log('Get images...');
-        getLogos(list, __dirname + '/public/imgs/', function () {
-            fs.writeFileSync(__dirname + '/public/sources-dist-stable.json', JSON.stringify(data, null, 2));
-            fs.writeFileSync(__dirname + '/public/sources-dist.json', JSON.stringify(data, null, 2));
+            console.log('Get images...');
+            getLogos(list, __dirname + '/public/imgs/', function () {
+                fs.writeFileSync(__dirname + '/public/sources-dist-stable.json', JSON.stringify(data, null, 2));
+                fs.writeFileSync(__dirname + '/public/sources-dist.json', JSON.stringify(dataLatest, null, 2));
 
-            done();
+                done();
+            });
         });
     });
 }
 
+function callInstall(done) {
+    exec('npm i --production --ignore-scripts', {
+        cwd: __dirname + '/ioBroker'
+    }, function (err, stdout, stderr) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        } else {
+            console.log('install all packages DONE');
+        }
+        done();
+    });
+}
 function activateLocalNpm(done) {
     // npm cache clean
     console.log('Clean cache...');
@@ -119,28 +143,24 @@ function activateLocalNpm(done) {
                 directory: __dirname + '/db'
             });
 
-            console.log('install all packages...');
-            exec('npm i --production --ignore-scripts', {
-                cwd: __dirname + '/ioBroker'
-            }, function (err, stdout, stderr) {
-                if (err) {
-                    console.error(err);
-                    process.exit(1);
-                } else {
-                    console.log('install all packages DONE');
-                }
-
-                // work with result
-                console.log('npm set registry remote...');
-                exec('npm set registry https://registry.npmjs.org', function (err, stdout, stderr) {
-                    if (err) {
-                        console.error(err);
-                        process.exit(1);
-                    } else {
-                        console.log('npm set registry remote DONE');
-                    }
-                    localNpm.shutdown(); // something calls process exit inside
-                    done();
+            console.log('install all latest packages...');
+            fs.writeFileSync(__dirname + '/ioBroker/package.json', fs.readFileSync(__dirname + '/ioBroker/package-latest.json'));
+            callInstall(() => {
+                fs.writeFileSync(__dirname + '/ioBroker/package.json', fs.readFileSync(__dirname + '/ioBroker/package-stable.json'));
+                callInstall(() => {
+                    // work with result
+                    console.log('npm set registry remote...');
+                    exec('npm set registry https://registry.npmjs.org', function (err, stdout, stderr) {
+                        if (err) {
+                            console.error(err);
+                            process.exit(1);
+                        } else {
+                            console.log('npm set registry remote DONE');
+                        }
+                        console.log('Now you can zip: zip -r localNpm.zip ../localNpmRepo');
+                        localNpm.shutdown(); // something calls process exit inside
+                        done();
+                    });
                 });
             });
         });
